3fbe6efb8e2ebcb3574b72d0e826ab7b224877fd
aznf.lubis <arlyzeina24@gmail.com>
2022-11-15 06:40:02 +0700

    config

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const DB_URL = 'mongodb://localhost:27017/tpa5'
+const DB_URL = 'mongodb://localhost:27017/toDoList'
 
 const db = mongoose.connect(DB_URL)
 
